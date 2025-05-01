0be97243e2ee151b79175ce6f7319ae6902f1153
Abdul Haadi <ahaadi1001@gmail.com>
2025-05-02 03:50:11 +0500

    kk

--- src/components/sections/Portfolio.tsx
+++ src/components/sections/Portfolio.tsx
@@ -5,7 +5,7 @@ const projects = [
   {
     id: 1,
     image: 'https://images.unsplash.com/photo-1728044849277-9cb3cd94e729?q=80&w=1934&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-    link: 'https://honda-tcj.netlify.app/',
+    link: 'https://honda-tcj.netlify.app',
   },
   {
     id: 2,
